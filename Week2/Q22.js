588a457b3642a134e82d4405c2d90744c4ea7eb1
iamvsc04 <saicharanstar22@gmail.com>
2025-01-17 22:12:20 +0530

    -

--- Week2/Q22.js
+++ Week2/Q22.js
@@ -1,25 +1,25 @@
-function* seperateSequences(seq1) {
-    let seq2 = [0, 1];
-    let idx = 0,
-      idx2 = 0;
-    while (true) {
-      if (idx < seq1.length) {
-        yield seq1[idx++];
-      } else {
+function* seperateSequences(seq1, seq2) {
+  let idx = 0,
+    idx2 = 0;
+  while (true) {
+    if (idx < seq1.length) {
+      if (idx == 0) console.log("First Sequence:");
+      yield seq1[idx++];
+    } else {
+      if (idx2 < seq2.length) {
         if (idx == seq1.length) {
-          console.log("fibonacci starts here !!");
+          console.log("Second Sequence starts here !!");
           idx++;
         }
-        let fib = seq2[seq2.length - 1] + seq2[seq2.length - 2];
-        seq2.push(fib);
         yield seq2[idx2++];
       }
     }
   }
-  let seq1 = [1, 2, 3, 4, 5, 6];
-  const gen = seperateSequences(seq1);
-  
-  for (let i = 0; i < 10; i++) {
-    console.log(gen.next().value);
-  }
-  
+}
+let seq1 = [1, 2, 3, 4, 5, 6];
+let seq2 = [0, 1, 1, 2, 3, 5, 8, 13, 21];
+const gen = seperateSequences(seq1, seq2);
+
+for (let i = 0; i < 12; i++) {
+  console.log(gen.next().value);
+}
